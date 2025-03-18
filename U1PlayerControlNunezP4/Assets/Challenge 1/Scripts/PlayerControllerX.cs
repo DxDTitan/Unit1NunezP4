6fd4aa7041029b833430d78d3370dc8e6a7484c6
AngelN <741865@student.dusd.net>
2025-03-18 11:45:43 -0700

    Planes
    Added  planes

---
+++ U1PlayerControlNunezP4/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 50.0f;
+    public float horizontalInput;
+    public float verticalInput;
+    private float rotationSpeed = 40.0f;
+    public float 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed * verticalInput);
+
+
+    }
+}
